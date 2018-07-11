cafba54a72aaa499e2d2599870392a66c8fe29d1
slavahokage <slava123smirnov@gmail.com>
2018-07-11 12:46:13 +0300

    adapter pattern

---
+++ src/SquarePegAdapter.java
@@ -0,0 +1,18 @@
+/**
+ * Created by USER on 11.07.2018.
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Рассчитываем минимальный радиус, в который пролезет этот колышек.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
